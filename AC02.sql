96321a3a158c9ed015d34ddf7eead01df84ed923
João Pedro Baria <53881642+JPBariaFIT@users.noreply.github.com>
2019-09-04 07:05:46 -0300

    AC02.sql - FIT - LSQL (03/09/2019)

---
+++ AC02.sql
@@ -0,0 +1,26 @@
+-- João Pedro Baria RA: XXXXXXX
+-- Lucas Buchalla Sesti RA: XXXXXXX
+
+CREATE TABLE PEDIDO (
+    codigo INT INDENTITY,
+    data SMALLDATETIME NOT NULL,
+    CONSTRAINT pkPedido PRIMARY KEY(codigo)
+);
+
+CREATE TABLE PRODUTO (
+  codigo INT INDENTITY,
+  nome VARCHAR(120) NOT NULL,
+  preco SMALLMONEY NOT NULL,
+  CONSTRAINT pkProduto PRIMARY KEY(codigo)
+);
+
+CREATE TABLE ITEM (
+  cod_pedido INT, 
+  cod_produto INT,
+  numero INT NOT NULL,
+  qtd SMALLINT NOT NULL,
+  preco SMALLMONEY,
+  CONSTRAINT fkCod_pedido FOREIGN KEY(cod_pedido) REFERENCES PEDIDO(codigo) 
+  CONSTRAINT fkCod_produto FOREIGN KEY(cod_produto) REFERENCES PRODUTO(codigo)
+  CONSTRAINT pkNumero PRIMARY KEY(numero, cod_pedido, cod_produto) 
+);
